f2b34ef19e01dfe2559e3301b57f34b4776827c0
javierdillon21 <javierdillon21@gmail.com>
2021-10-08 21:12:33 -0500

    update meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,14 +11,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="application-name" content="PWA App" />
+          <meta name="application-name" content="Hupro" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
             content="default"
           />
-          <meta name="apple-mobile-web-app-title" content="PWA App" />
-          <meta name="description" content="Best PWA App in the world" />
+          <meta name="apple-mobile-web-app-title" content="Hupro" />
+          <meta name="description" content="App Hupro Humane Propiedades" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta
@@ -50,13 +50,13 @@ class MyDocument extends Document {
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/icons/favicon-32x32.png"
+            href="/icons/favicon.ico"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/icons/favicon-16x16.png"
+            href="/icons/favicon.ico"
           />
           <link rel="manifest" href="/manifest.json" />
           {/* <link
@@ -70,27 +70,33 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
           />
 
-          {/* <meta name="twitter:card" content="summary" />
-          <meta name="twitter:url" content="https://yourdomain.com" />
-          <meta name="twitter:title" content="PWA App" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://hupro.vercel.app" />
+          <meta name="twitter:title" content="Hupro" />
           <meta
             name="twitter:description"
-            content="Best PWA App in the world"
+            content="App Hupro Humane Propiedades"
           />
           <meta
             name="twitter:image"
-            content="https://yourdomain.com/icons/android-chrome-192x192.png"
+            content="https://hupro.vercel.app/icons/HUPRO LOGO.png"
           />
-          <meta name="twitter:creator" content="@DavidWShadow" />
+          {/* <meta name="twitter:creator" content="@DavidWShadow" /> */}
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="PWA App" />
-          <meta property="og:description" content="Best PWA App in the world" />
-          <meta property="og:site_name" content="PWA App" />
+          <meta property="og:title" content="Hupro" />
+          <meta
+            property="og:description"
+            content="App Hupro Humane Propiedades"
+          />
+          <meta
+            property="og:site_name"
+            content="App Hupro Humane Propiedades"
+          />
           <meta property="og:url" content="https://yourdomain.com" />
           <meta
             property="og:image"
-            content="https://yourdomain.com/icons/apple-touch-icon.png"
-          /> */}
+            content="https://hupro.vercel.app/icons/HUPRO LOGO.png"
+          />
         </Head>
         <body>
           <Main />
